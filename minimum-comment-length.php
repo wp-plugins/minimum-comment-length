53d1380cc8c2f9d7ff1bda67a920a23306418845
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-15 16:27:33 +0000

    Added the option for the admin not to be blocked, added settings link and Ozh admin menu icon
    git-svn-id: https://plugins.svn.wordpress.org/minimum-comment-length/tags/0.6@80276 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ minimum-comment-length.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name: Minimum Comment Length
+Version: 0.6
+Plugin URI: http://yoast.com/wordpress/minimum-comment-length/
+Description: Check the comment for a set minimum length and disapprove it if it's too short.
+Author: Joost de Valk
+Author URI: http://yoast.com/
+
+Copyright 2008 Joost de Valk (email: joost@yoast.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+if ( ! class_exists( 'MinComLength_Admin' ) ) {
+
+	class MinComLength_Admin {
+		
+		function add_config_page() {
+			global $wpdb;
+			if ( function_exists('add_submenu_page') ) {
+				add_options_page('Min Comment Length Configuration', 'Min Comment Length', 10, basename(__FILE__), array('MinComLength_Admin','config_page'));
+				add_filter( 'plugin_action_links', array( 'MinComLength_Admin', 'filter_plugin_actions'), 10, 2 );
+				add_filter( 'ozh_adminmenu_icon', array( 'MinComLength_Admin', 'add_ozh_adminmenu_icon' ) );								
+			}
+		}
+
+		function add_ozh_adminmenu_icon( $hook ) {
+			static $mclicon;
+			if (!$mclicon) {
+				$mclicon = WP_CONTENT_URL . '/plugins/' . plugin_basename(dirname(__FILE__)). '/comment_edit.png';
+			}
+			if ($hook == 'minimum-comment-length.php') return $mclicon;
+			return $hook;
+		}
+
+		function filter_plugin_actions( $links, $file ){
+			//Static so we don't call plugin_basename on every plugin row.
+			static $this_plugin;
+			if ( ! $this_plugin ) $this_plugin = plugin_basename(__FILE__);
+			
+			if ( $file == $this_plugin ){
+				$settings_link = '<a href="options-general.php?page=minimum-comment-length.php">' . __('Settings') . '</a>';
+				array_unshift( $links, $settings_link ); // before other links
+			}
+			return $links;
+		}
+		
+		function config_page() {
+			// Set some defaults if no settings are set yet
+			$options['mincomlength'] = 15;
+			$options['mincomlengtherror'] = "Error: Your comment is too short. Please try to say something useful.";
+			add_option('MinComLengthOptions', $options);
+			
+			// Overwrite defaults with saved settings
+			if ( isset($_POST['submit']) ) {
+				if (!current_user_can('manage_options')) die(__('You cannot edit the Minimum Comment Length options.'));
+				check_admin_referer('mincomlength-config');
+
+				if (isset($_POST['mincomlength']) && $_POST['mincomlength'] != "" && is_numeric($options['mincomlength'])) 
+					$options['mincomlength'] = $_POST['mincomlength'];
+
+				if (isset($_POST['mincomlengtherror']) && $_POST['mincomlengtherror'] != "") 
+					$options['mincomlengtherror'] = $_POST['mincomlengtherror'];
+
+				update_option('MinComLengthOptions', $options);
+			}
+			
+			$options = get_option('MinComLengthOptions');			
+			?>
+			<div class="wrap">
+				<h2>Minimum Comment Length options</h2>
+				<form action="" method="post" id="mincomlength-conf">
+					<?php
+					if ( function_exists('wp_nonce_field') )
+						wp_nonce_field('mincomlength-config');
+					?>
+					<table class="form-table" style="width: 100%;">
+						<tr valign="top">
+							<th scrope="row">
+								<label for="mincomlength">Minimum comment length:</label>
+							</th>
+							<td>
+								<input type="text" value="<?php echo $options['mincomlength']; ?>" name="mincomlength" id="mincomlength" size="4"/>
+							</td>
+						</tr>
+						<tr valign="top">
+							<th scrope="row">
+								<label for="mincomlengtherror">Error message:</label>
+							</th>
+							<td>
+								<input type="text" value="<?php echo $options['mincomlengtherror']; ?>" name="mincomlengtherror" id="mincomlengtherror" size="50"/>
+							</td>
+						</tr>
+
+					</table>
+					<p class="submit"><input type="submit" name="submit" value="Update Settings &raquo;" /></p>
+				</form>
+			</div>
+<?php		}	
+	}
+}
+
+function check_comment_length($commentdata) {
+	$options = get_option('mincomlength');
+	if (!isset($options['mincomlength']) || $options['mincomlength'] == "" || !is_numeric($options['mincomlength']))
+		$options['mincomlength'] = 15;
+
+	if (!isset($options['mincomlengtherror']) || $options['mincomlengtherror'] == "")
+		$options['mincomlengtherror'] = "Error: Your comment is too short. Please try to say something useful.";
+	
+	if (current_user_can('edit_users')) {
+		return $commentdata;
+	}
+	if (strlen(trim($commentdata['comment_content'])) < $options['mincomlength']) {
+		wp_die( __($options['mincomlengtherror']) );
+	} else {
+		return $commentdata;
+	}
+}
+
+add_filter('preprocess_comment','check_comment_length');
+add_action('admin_menu', array('MinComLength_Admin','add_config_page'));
+?>
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Minimum Comment Length ===
+Contributors: joostdevalk
+Donate link: http://yoast.com/donate/
+Tags: comments, spam
+Requires at least: 2.2
+Tested up to: 2.7
+stable tag: 0.6
+
+Check the comment for a set minimum length and disapprove it if it's too short.
+
+== Description ==
+
+Don't you find it annoying when someone comments with just a smiley? Or a "thanks"? On quite a few forums you have to leave a comment with a minimum of 15 characters to be able to comment, but WordPress doesn't allow you to set such a threshold. With this plugin, it does. It easily checks the comment for a set minimum length and disapprove it if it's too short (except if you're the admin).
+
+More info:
+
+* [Minimum Comment Length](http://yoast.com/wordpress/minimum-comment-length/).
+* Check out the other [Wordpress plugins](http://yoast.com/wordpress/) by the same author.
+
+== Installation ==
+
+Installation is easy:
+
+* Download the plugin.
+* Copy the folder `minimum-comment-length` to the plugins directory of your blog.
+* Enable the plugin in your admin panel.
+
+Optional:
+
+* An options panel will appear under Options.
+* Decide on which comment length you'd like to block and what error to throw when a comment is too short.
